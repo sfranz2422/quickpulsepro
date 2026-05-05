62d8a562d07d420ada7ae2f646b36fc99db700ad
sfranz2422 <stephenfranz22@gmail.com>
2026-05-05 14:15:25 -0400

    debug fix

--- quickpulsepro/settings.py
+++ quickpulsepro/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get(
--- quickpulsepro/urls.py
+++ quickpulsepro/urls.py
@@ -17,13 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
-
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("", RedirectView.as_view(pattern_name="dashboard", permanent=False)),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
 
